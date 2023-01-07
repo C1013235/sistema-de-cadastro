6f66eb272e9a794afc0bc4e244c6a6ae5b013da9
C1013235 <120650542+C1013235@users.noreply.github.com>
2023-01-07 11:47:03 -0300

    Agora feito na web

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 console.log ("Rodando o sistema de gerenciamento de jogos")
 console.log ("Testando mudanças de código na web e no local. Este na web 3")
 console.log ("Testando novamente")
+console.log ("Testando de novo")
+
