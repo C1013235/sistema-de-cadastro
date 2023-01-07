ba6cf45b8e25bf4b0f34eeaa74f62efaf928bbe1
C1013235 <120650542+C1013235@users.noreply.github.com>
2023-01-07 11:49:47 -0300

    novo outro teste

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ console.log ("Rodando o sistema de gerenciamento de jogos")
 console.log ("Testando mudanças de código na web e no local. Este na web 3")
 console.log ("Testando novamente")
 console.log ("Testando de novo")
+console.log ("Outro teste")
+
 
