63f53dd8a9cd7d2a8bd122ac825a4f83176a3c50
Fabio Velloso Wolff <fabio.wolff@coopersystem.com.br>
2023-01-07 11:33:25 -0300

    Teste commit

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
 console.log ("Rodando o sistema de gerenciamento de jogos")
-console.log ("Testando mudanças de código na web e no local. Este na web")
+console.log ("Testando mudanças de código na web e no local. Este na web 2")
