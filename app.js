662f6c54994aee63d0b8c4b93bf18c18dcfeb5b0
Fabio Velloso Wolff <fabio.wolff@coopersystem.com.br>
2023-01-07 15:26:08 -0300

    corrigindo o app js

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ console.log ("Testando mudanças de código na web e no local. Este na web 3")
 console.log ("Testando novamente")
 console.log ("Testando de novo")
 console.log ("Outro teste")
+console.log ("Contato agora")
 
 
